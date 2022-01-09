0387bdd0bff631a5efcf2a93fccaf3c06e3b8aa2
FunesLucas <funeslucas@outlook.com>
2022-01-09 03:07:23 -0300

    Implementando Dashboard : Cantidad  Fabricantes

--- src/dashboard/src/components/fabricantes/Fabricantes.js
+++ src/dashboard/src/components/fabricantes/Fabricantes.js
@@ -1,19 +1,56 @@
 import React from 'react';
 import "./Fabricantes.css"
+import { Component } from "react";
 
-function Fabricantes() {
-    return (
-        <div>
-            <p>Fabricantes:</p>
-            <div className='container-fabricantes'>
 
-                <div className='container-tot-fabricantes'>
-                    <h3>Cantidad de Fabricantes</h3>
-                </div>
-             
-            </div>
-        </div>
-     );
-  }
+    class Fabricantes extends Component {
+        constructor(props) {
+          super(props);
+          this.state = {
+            listadoFabricantes: [],
+          };
+        }
+      
+        componentWillMount() {
+          fetch("https://besttires.herokuapp.com/api/fabricantes")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+              this.setState({ listadoFabricantes: data });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+        render() {
+          let productos = this.state.listadoFabricantes;
+          let contenido;
+          if (productos.length === 0) {
+            contenido = "Cargando...";
+          } else {
+            contenido = productos.count;
+          }
+      
+          return (
+              <div>
+                  <p>Fabricantes:</p>
+                  <div className='container-fabricantes'>
+
+                      <div className='container-tot-fabricantes'>
+                          <h3>Cantidad de Fabricantes</h3>
+                          <h5>{contenido}</h5>
+                      </div>
+
+                  </div>
+              </div>
+          );
+        }
+      }
+
 
 export default Fabricantes;
+
+
+
+        
+
